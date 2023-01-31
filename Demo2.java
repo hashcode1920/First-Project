f63f5583bfaf3ce38601fd80e4dfb31dc012c309
hashcode1920 <123951174+hashcode1920@users.noreply.github.com>
2023-01-31 12:30:52 +0530

    Create Demo2.java

---
+++ Demo2.java
@@ -0,0 +1,10 @@
+class Demo2{
+ public static void main(String[] args){
+int a,b;
+   Scanner sc = new Scanner(System.in);
+   a = sc.nextInt();
+   b = sc.nextInt();
+   System.out.printf("%d + %d"a,b);
+   
+ }
+}
