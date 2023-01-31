3f0372e3122111d1c8a9306e3046423170171beb
hashcode1920 <123951174+hashcode1920@users.noreply.github.com>
2023-01-31 12:38:34 +0530

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 class Demo2{
  public static void main(String[] args){
 int a,b;
    Scanner sc = new Scanner(System.in);
    a = sc.nextInt();
    b = sc.nextInt();
-   System.out.printf("%d + %d"a,b);
+   System.out.printf("%d + %d = %d",a,b,a+b);
    
  }
 }
