c06b12c89543d977cdbaf041c0125628215f826a
hashcode1920 <nipunkbhardwaj73@gmail.com>
2023-02-02 14:42:30 +0530

    New files were added

---
+++ src/fullName.java
@@ -0,0 +1,12 @@
+public class fullName {
+
+    static int myMethod(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(5, 3));
+        System.out.println(myMethod(6, 3));
+        System.out.println(myMethod(7, 3));
+    }
+}
