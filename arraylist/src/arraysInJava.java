6116f4ee1e3f7fdb8698f84a0eeb8f72d7939bb1
hashcode1920 <123951174+hashcode1920@users.noreply.github.com>
2023-04-26 14:09:22 +0530

    Add files via upload

---
+++ arraylist/src/arraysInJava.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arraysInJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size");
+        int size = sc.nextInt();
+        int[] marks = new int[size];
+
+        //input:
+
+        for (int j=0; j<size; j++) {
+            marks[j] = sc.nextInt();
+        }
+        for (int j =0;j< marks.length;j++) {
+            System.out.println(marks[j]);
+        }
+        System.out.println("please enter value that you want from marks :  ");
+        int x = sc.nextInt();
+        //output:
+        for(int i=0; i<marks.length; i++){
+            if(marks[i] == x) {
+                System.out.println("x found at index : " + i);
+            }
+        }
+    }
+}
