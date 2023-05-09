f5d4937c9a1a53f65ba8870d2e41d539700e658c
hashcode1920 <123951174+hashcode1920@users.noreply.github.com>
2023-05-09 17:58:17 +0530

    Update FloydTriangle.java

--- main/src/FloydTriangle.java
+++ main/src/FloydTriangle.java
@@ -2,10 +2,22 @@ public class FloydTriangle {
     public static void main(String[] args) {
         int num = 5;
         int number = 1;
+        int num2 = 4;
+
 
         for (int i=1; i<=num; i++){
             for (int j=1; j<=i; j++){
-                System.out.print(number+" ");
+                System.out.print(number+"   ");
+                if(number<10) {
+                    System.out.print(" ");
+                }
+                number++;
+            }
+            System.out.println();
+        }
+        for (int i = num2; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number+"   ");
                 number++;
             }
             System.out.println();
